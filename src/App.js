30890b199e4af1d40e0fff3b863eb2e5fa041455
Andrea-Pinales <95321339+Andrea-Pinales@users.noreply.github.com>
2022-03-08 20:05:54 -0800

    Added H1 to code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h1>
+        Your <em>free</em>
+        <br />
+        dictionary
+      </h1>
+    </div>
+  );
 }
 
 export default App;
